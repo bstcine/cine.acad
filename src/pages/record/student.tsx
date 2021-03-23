ceef9935b2b6720f839c8c43ba220ee61987e1f0
daweilv <lvdawei1970@gmail.com>
2021-03-23 21:49:55 +0800

    feat: record page

--- src/pages/record/student.tsx
+++ src/pages/record/student.tsx
@@ -3,5 +3,10 @@ import Header from '@/components/header';
 import { NavLink } from 'umi';
 
 export default () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="title">xxx老师的上课反馈</div>
+      <div className="content"></div>
+    </div>
+  );
 };
--- src/pages/record/tutor.tsx
+++ src/pages/record/tutor.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button, DatePicker } from 'antd';
+import style from './style.less';
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 12 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 export default () => {
+  const [form] = Form.useForm();
+  const [duration, setDuration] = useState('0');
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  const onValuesChange = (changedValues: any, allValues: any) => {
+    console.log('onValuesChange:', changedValues, allValues);
+    if ('time' in changedValues) {
+      const [start, end] = changedValues.time;
+      setDuration(end.diff(start, 'hours', true).toFixed(1));
+    }
+  };
+
   return (
     <div className="container">
-      <div className="title">xxx老师的上课反馈</div>
-      <div className="content"></div>
+      <div className={style.title}>Richard's tutorial records</div>
+      <div className="content">
+        <Form
+          {...layout}
+          name="basic"
+          form={form}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          onValuesChange={onValuesChange}
+        >
+          <Form.Item
+            label="Time arrange"
+            name="time"
+            rules={[{ required: true, message: 'Please fill time arrange!' }]}
+            extra={`${duration} Hours`}
+          >
+            <RangePicker
+              showTime
+              style={{ width: '100%' }}
+              onOpenChange={(a) => {
+                console.log(a);
+                const time = form.getFieldValue('time');
+                console.log(time);
+              }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Course Content"
+            name="content"
+            rules={[{ required: true, message: 'Please input course content!' }]}
+          >
+            <TextArea rows={10} />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
