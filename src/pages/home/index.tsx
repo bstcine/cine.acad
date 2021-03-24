bb1268e79a857860c454b31e4a250eb172f9a19b
daweilv <lvdawei1970@gmail.com>
2021-03-25 01:18:38 +0800

    feat: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -77,6 +77,20 @@ export default () => {
           ))}
         </div>
       </main>
+      <div className="more-infos">
+        <div className="container">
+          <div className="more-info">
+            <div className="icon">
+              <i className="far fa-handshake"></i>
+            </div>
+            <div className="info">
+              <p>善恩所有课程和服务高度透明。所有一对一服务，任意时间无条件退款。</p>
+              <p>适合，则合作；不适合，则离开。</p>
+              <p>不强求，来去自由；不欺骗，没有伤害。</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <FooterInfo />
       <Copyright />
     </>
