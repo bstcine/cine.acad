25a99f2cbabc1bb0f3b23fdc4f4f070e5e0bc1ce
daweilv <lvdawei1970@gmail.com>
2021-03-25 22:02:19 +0800

    feat: about page

--- src/pages/record/student.tsx
+++ src/pages/record/student.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import Header from '@/components/header';
-import { NavLink } from 'umi';
+import style from './style.less';
 
 export default () => {
   return (
     <div className="container">
-      <div className="title">xxx老师的上课反馈</div>
-      <div className="content"></div>
+      <div className={style.title}>xxx老师的上课反馈</div>
+      <div className="content">
+        <div className={style.itemControl}>
+          <div className={style.label}>上课时间</div>
+          <div className={style.item}>2021-03-12 12:00:00 ~ 2021-03-12 14:00:00</div>
+        </div>
+        <div className={style.itemControl}>
+          <div className={style.label}>授课时长</div>
+          <div className={style.item}>1.5小时</div>
+        </div>
+        <div className={style.itemControl}>
+          <div className={style.label}>授课内容</div>
+          <div className={style.item}>
+            ●
+            自主学习能力较好的孩子，建议每次花30-40分钟，观看视频课程、做笔记并完成相应的练习，如若每周可以至少学习2-3次，保持一定的频率和学习强度，效果更佳。
+            ●
+            如果孩子希望有互动，有老师的启发点拨，也可以考虑善恩在线私塾服务，由高水平的北美导师1对1远程授课。具体可以咨询Alice老师（微信号iyuan1023)。
+          </div>
+        </div>
+        <div className={style.itemControl}>
+          <div className={style.label}>上课质量打分</div>
+          <div className={style.item}></div>
+        </div>
+        <div className={style.itemControl}>
+          <div className={style.label}>给老师的评语</div>
+          <div className={style.item}></div>
+        </div>
+      </div>
     </div>
   );
 };
