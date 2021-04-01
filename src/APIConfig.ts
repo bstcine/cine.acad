51cdb48b28db56d6d3b100cc41449a9d20d76cb5
daweilv <lvdawei1970@gmail.com>
2021-04-01 11:53:03 +0800

    feat: host

--- src/APIConfig.ts
+++ src/APIConfig.ts
@@ -1,5 +1,5 @@
-// const host = 'https://www.bstcine.com';
-const host = 'http://localhost:9000';
+const host = 'https://www.bstcine.com';
+// const host = 'http://localhost:9000';
 
 export const APIURL_Acad_List = host + '/api/acad/list'; // 善恩学院列表页面
 export const APIURL_Acad_Tutor = host + '/api/acad/tutor'; // 善恩学院导师查询
