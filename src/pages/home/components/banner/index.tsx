b1fd0412d34df7e991bd74761dbce4cc13c22a24
daweilv <lvdawei1970@gmail.com>
2021-04-09 19:02:32 +0800

    feat: home样式调整

--- src/pages/home/components/banner/index.tsx
+++ src/pages/home/components/banner/index.tsx
@@ -17,38 +17,38 @@ function Banner() {
       <div className={bannerStyle.overlay} />
 
       <div className={bannerStyle.text}>
-        <Carousel
-          autoPlay
-          infiniteLoop
-          emulateTouch={false}
-          centerMode={false}
-          showStatus={false}
-          showArrows={false}
-          showThumbs={false}
-          showIndicators={false}
-          transitionTime={500}
-        >
-          <p>
-            走进名校
-            <br />
-            实力顾问
-          </p>
-          <p>
-            学科辅导
-            <br />
-            能力提升
-          </p>
-          <p>
-            助力学业
-            <br />
-            升学顾问
-          </p>
-          <p>
-            好导师，
-            <br />
-            尽在善恩！
-          </p>
-        </Carousel>
+        {/*<Carousel*/}
+        {/*  autoPlay*/}
+        {/*  infiniteLoop*/}
+        {/*  emulateTouch={false}*/}
+        {/*  centerMode={false}*/}
+        {/*  showStatus={false}*/}
+        {/*  showArrows={false}*/}
+        {/*  showThumbs={false}*/}
+        {/*  showIndicators={false}*/}
+        {/*  // transitionTime={5}*/}
+        {/*>*/}
+        {/*  <p>*/}
+        {/*    走进名校*/}
+        {/*    <br />*/}
+        {/*    实力顾问*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    学科辅导*/}
+        {/*    <br />*/}
+        {/*    能力提升*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    助力学业*/}
+        {/*    <br />*/}
+        {/*    升学顾问*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    好导师，*/}
+        {/*    <br />*/}
+        {/*    尽在善恩！*/}
+        {/*  </p>*/}
+        {/*</Carousel>*/}
       </div>
 
       {/*<div className={bannerStyle.slogan}>CINE ACADEMY</div>*/}
