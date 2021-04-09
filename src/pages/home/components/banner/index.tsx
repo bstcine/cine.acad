222a51ee3b46a3bf325458d4257dfadd114774fa
daweilv <lvdawei1970@gmail.com>
2021-04-09 18:19:25 +0800

    feat: home样式调整

--- src/pages/home/components/banner/index.tsx
+++ src/pages/home/components/banner/index.tsx
@@ -16,44 +16,44 @@ function Banner() {
       </div>
       <div className={bannerStyle.overlay} />
 
-      {/*<div className={bannerStyle.text}>*/}
-      {/*  <Carousel*/}
-      {/*    autoPlay*/}
-      {/*    infiniteLoop*/}
-      {/*    emulateTouch={false}*/}
-      {/*    centerMode={false}*/}
-      {/*    showStatus={false}*/}
-      {/*    showArrows={false}*/}
-      {/*    showThumbs={false}*/}
-      {/*    showIndicators={false}*/}
-      {/*  >*/}
-      {/*    <p>*/}
-      {/*      哈佛、MIT*/}
-      {/*      <br/>*/}
-      {/*      哥伦比亚*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      汇聚*/}
-      {/*      <br/>*/}
-      {/*      世界名师*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      在家里*/}
-      {/*      <br/>*/}
-      {/*      学美高课程*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      学科辅导*/}
-      {/*      <br/>*/}
-      {/*      升学顾问*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      阅读和写作*/}
-      {/*      <br/>*/}
-      {/*      能力提升*/}
-      {/*    </p>*/}
-      {/*  </Carousel>*/}
-      {/*</div>*/}
+      <div className={bannerStyle.text}>
+        <Carousel
+          autoPlay
+          infiniteLoop
+          emulateTouch={false}
+          centerMode={false}
+          showStatus={false}
+          showArrows={false}
+          showThumbs={false}
+          showIndicators={false}
+        >
+          <p>
+            哈佛、MIT
+            <br />
+            哥伦比亚
+          </p>
+          <p>
+            汇聚
+            <br />
+            世界名师
+          </p>
+          <p>
+            在家里
+            <br />
+            学美高课程
+          </p>
+          <p>
+            学科辅导
+            <br />
+            升学顾问
+          </p>
+          <p>
+            阅读和写作
+            <br />
+            能力提升
+          </p>
+        </Carousel>
+      </div>
 
       {/*<div className={bannerStyle.slogan}>CINE ACADEMY</div>*/}
     </div>
